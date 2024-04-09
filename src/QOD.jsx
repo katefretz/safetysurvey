59a4513d87b8c2e47094ca816819082a3face51f
katefretz <fret5380@mylaurier.ca>
2024-04-08 21:25:19 -0400

    add multiple questions

--- src/QOD.jsx
+++ src/QOD.jsx
@@ -7,24 +7,96 @@ export default function () {
     const questions = [{
                 type: "radiogroup",
                 name: "question1",
-                title: "What is the most common cause of lifeguard injuries?",
+                title: "What is the safest place to be during an earthquake?",
                 choices: [
-                    "Slip and fall accidents", "Pool chemicals", "Sunburns", "Sharks"
+                    "Inside a car", "Under a doorway", "Under a steardy table or desk", "Outside in an open area"
                 ],
-                correctAnswer: "Slip and fall accidents"
+                correctAnswer: "Under a steardy table or desk"
             },
             {
                 type: "radiogroup",
                 name: "question2",
-                title: "How much sunscreen should you apply?",
+                title: "What should you do if you are outside during a thunderstorm?",
                 choices: [
-                    "teaspoon", "tablespoon", "quart", "gallon"
+                    "Stand under a tree", "Lay flat on the ground", "Seek shelter immediately", "Run around outside"
                 ],
-                correctAnswer: "tablespoon"
+                correctAnswer: "Seek shelter immediately"
+            },
+            {
+                type: "radiogroup",
+                name: "question3",
+                title: "How do you know a tornado may be imminent?",
+                choices: [
+                    "Greenish sky", "Heavy rain", "Hail", "All of the above"
+                ],
+                correctAnswer: "All of the above"
+            },
+            {
+                type: "radiogroup",
+                name: "question4",
+                title: "What is the recommended way to protect yourself during a wildfire if evacuation is not possible?",
+                choices: [
+                    "Hide in a basement", "Find safety in a large body of water", "Stay in a room with windows and doors closed", "Run away from the flames"
+                ],
+                correctAnswer: "Stay in a room with windows and doors closed"
+            },
+            {
+                type: "radiogroup",
+                name: "question5",
+                title: "What is the NOT a recommended saftey measure during a hurricane?",
+                choices: [
+                    "Boarding up windows", "Using candles as a light source during power outages", "Evacuating to higher ground if in a flood-prone area", "Securing outdoor objects that could become projectables in high winds"
+                ],
+                correctAnswer: "Using candles as a light source during power outages"
+            },
+            {
+                type: "radiogroup",
+                name: "question6",
+                title: "What is the primary cause of tsunamis?",
+                choices: [
+                    "Earthquakes", "Volcanic eruptions", "Hurricanes", "Tornados"
+                ],
+                correctAnswer: "Earthquakes"
+            },
+            {
+                type: "radiogroup",
+                name: "question7",
+                title: "What should you do if you are driving and encounter a flooded road?",
+                choices: [
+                    "Speed up and drive through the water", "Turn around and find an alternative route", "Wait for emergency responders to rescue you", "Proceed with caution and honk your horn to warn others"
+                ],
+                correctAnswer: "Turn around and find an alternative route"
+            },
+            {
+                type: "radiogroup",
+                name: "question8",
+                title: "What should you do during an earthquake if you are indoors and cannot reach a peice of furniture to take cover?",
+                choices: [
+                    "Stand in the doorway", "Stand in the center of the room", "Lay flat on the ground and cover your head with your arms", "Run outside"
+                ],
+                correctAnswer: "Lay flat on the ground and cover your head with your arms"
+            },
+            {
+                type: "radiogroup",
+                name: "question9",
+                title: "What is a precautionary measure to take before a hurricane strikes?",
+                choices: [
+                    "Trim trees and shrubs", "Open all of the windows", "Stock up on candels", "Park your car under a tree for extra protection"
+                ],
+                correctAnswer: "Trim trees and shrubs"
+            },
+            {
+                type: "radiogroup",
+                name: "question10",
+                title: "What is the safest course of action if you are outside and an earthquake occurs?",
+                choices: [
+                    "Lay flat on the ground and cover your head with your arms", "Run toward the nearest building for shelter", "Find an open area away from buildings, trees, and powerlines", "Climb up a hill for elevated safety"
+                ],
+                correctAnswer: "Find an open area away from buildings, trees, and powerlines"
             }];
     const nQuestion = Math.floor((Math.random() * questions.length));
     const surveyJson = {
-        title: "Lifeguard Safety",
+        title: "Natural Disaster Safety",
         showCorrectAnswer: "always",
         showProgressBar: "bottom",
         firstPageIsStarted: true,
@@ -32,7 +104,7 @@ export default function () {
         pages: [{
             elements: [{
                 type: "html",
-                html: "You are about to start a quiz on Lifeguard Safety. <br>You will have 30 seconds for every question and 60 seconds to end the quiz.<br>Enter your name below and click <b>Start Quiz</b> to begin."
+                html: "You are about to start a quiz on Natural Disaster Safety. <br>You will have 30 seconds for every question and 5 minutes to end the quiz.<br>Enter your name below and click <b>Start Quiz</b> to begin."
             }, {
                 type: "text",
                 name: "username",
